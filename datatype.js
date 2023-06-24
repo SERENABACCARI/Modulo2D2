f6783ff5681c081b82af376ff2eb51a6beda6e65
SERENABACCARI <serena88_2011@libero.it>
2023-06-24 07:15:08 +0200

    modulo 2D2

---
+++ datatype.js
@@ -0,0 +1,36 @@
+//spiegazione dei principali datatype//
+
+//STRING e' delimitata da virgolette e al suo interno viene inserito del testo//
+
+let stringa = "esempio di string"
+console.log(typeof stringa)
+
+//BOOLEAN e' logica e ci dice se quello che abbiamo scritto all interno delle parentesi tonde e' vero o falso//
+
+let boolean = (10>8)
+console.log(typeof boolean)
+
+// UNDEFINED significa che manca proprio l'informazione e che quindi la Variabile  non esiste //
+
+let Squat = undefined
+
+console.log(typeof Squat)
+
+// NULL è un informazione, significa che alla variabile non e' stato assegnato nessun valore//
+
+let deadlift = (null)
+
+console.log(typeof deadlift)
+
+//OGGETTO sono un insieme di coppi chiave- valore 
+
+let bodychek = {
+    name: "serena";
+    age:35;
+    athlete: elite;
+    sheisanathlete:true
+
+
+}
+
+console.log(bodychek)
